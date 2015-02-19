02c7735f476c216781585d58351268cd63a3c173
Sylvain Rayé <sly@diglin.com>
2015-02-19 18:10:09 +0100

    [POSTYC-8] - Add Order Pending YC processor

---
+++ app/code/community/Swisspost/YellowCube/Model/Queue/Message/Handler/Action/Processor/Order/Pending.php
@@ -0,0 +1,100 @@
+<?php
+
+/**
+ * Liip
+ *
+ * @author      Sylvain Rayé <sylvain.raye at diglin.com>
+ * @category    yellowcube
+ * @package     Swisspost_yellowcube
+ * @copyright   Copyright (c) 2015 Liip
+ */
+
+use \YellowCube\WAB\Partner;
+use \YellowCube\WAB\Order;
+use \YellowCube\WAB\OrderHeader;
+use \YellowCube\WAB\Position;
+use \YellowCube\WAB\AdditionalService\BasicShippingServices;
+
+/**
+ * Class Swisspost_YellowCube_Model_Queue_Message_Handler_Action_Processor_Order
+ */
+class Swisspost_YellowCube_Model_Queue_Message_Handler_Action_Processor_Order_Pending
+    extends Swisspost_YellowCube_Model_Queue_Message_Handler_Action_ProcessorAbstract
+    implements Swisspost_YellowCube_Model_Queue_Message_Handler_Action_ProcessorInterface
+{
+    /**
+     * @param array $data
+     * @return $this
+     */
+    public function process(array $data)
+    {
+        $helper = Mage::helper('swisspost_yellowcube');
+        $helperTools = Mage::helper('swisspost_yellowcube/tools');
+
+        $shipment = Mage::getModel('sales/order_shipment')->load($data['order_id'], 'order_id');
+        $response = $this->getYellowCubeService()->getYCCustomerOrderStatus($data['order_id']);
+
+        try {
+            if (!is_object($response) || !$response->isSuccess()) {
+                $message = $helper->__('Order #%s could not be transmitted to YellowCube: "%s".', $data['order_id'], $response->getStatusText());
+                $shipment
+                    ->addComment($message, false, false)
+                    ->save();
+
+                Mage::log($message . "\n" . print_r($response, true), Zend_Log::ERR, Swisspost_YellowCube_Helper_Data::YC_LOG_FILE, true);
+                $helperTools->sendAdminNotification($message);
+
+                // Add again in the queue to have an up to date status
+                $this->getQueue()->send(Zend_Json::encode(array(
+                    'action' => Swisspost_YellowCube_Model_Synchronizer::SYNC_ORDER_UPDATE,
+                    'order_id' => $data['order_id']
+                )));
+            } else {
+                if (Mage::helper('swisspost_yellowcube')->getDebug()) {
+                    Mage::log(print_r($response, true), Zend_Log::DEBUG, Swisspost_YellowCube_Helper_Data::YC_LOG_FILE);
+                }
+
+                // find orders that have been processed
+                if ($response->isSuccess() && !$response->isPending() && !$response->isError()) {
+
+                    $goodsIssueList = $this->getYellowCubeService()->getYCCustomerOrderReply($data['order_id']);
+
+                    foreach ($goodsIssueList as $goodsIssue) {
+                        $header = $goodsIssue->getCustomerOrderHeader();
+                        $shipmentNo = $header->getPostalShipmentNo();
+
+                        // Multi packaging / shipping is not supported atm.
+                        if (!empty($shipmentNo)) {
+                            //shipping number contains a semicolon, post api supports multiple values
+                            $shippingUrl = 'http://www.post.ch/swisspost-tracking?formattedParcelCodes=' . $shipmentNo;
+                            break;
+                        }
+                    }
+
+                    if (!empty($goodsIssueList)) {
+                        //Add a message to the order history incl. link to shipping infos
+                        $message = $helper->__('Your order has been shipped. You can use the following url for shipping tracking: %s', $shippingUrl);
+
+                        $shipment
+                            ->addComment($helper->__($message), true, true)
+                            ->save();
+
+                        //Update the order status to complete
+                        //commerce_order_status_update($order, 'complete');
+                    }
+                }
+            }
+        } catch (Exception $e) {
+            Mage::logException($e);
+            // Let's keep going further processes
+
+            // Add again in the queue to have an up to date status
+            $this->getQueue()->send(Zend_Json::encode(array(
+                'action' => Swisspost_YellowCube_Model_Synchronizer::SYNC_ORDER_UPDATE,
+                'order_id' => $data['order_id']
+            )));
+        }
+
+        return $this;
+    }
+}
