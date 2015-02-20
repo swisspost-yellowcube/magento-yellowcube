24b82a6d2302a42bee048f275b71f193f680a7e6
Sylvain Rayé <sly@diglin.com>
2015-02-20 14:58:48 +0100

    Prevent Order Update process to be run indefinitely

--- app/code/community/Swisspost/YellowCube/Model/Queue/Message/Handler/Action/Processor/Order/Update.php
+++ app/code/community/Swisspost/YellowCube/Model/Queue/Message/Handler/Action/Processor/Order/Update.php
@@ -1,12 +1,12 @@
 <?php
 
 /**
- * Liip
+ * Liip AG
  *
  * @author      Sylvain Rayé <sylvain.raye at diglin.com>
  * @category    yellowcube
  * @package     Swisspost_yellowcube
- * @copyright   Copyright (c) 2015 Liip
+ * @copyright   Copyright (c) 2015 Liip AG
  */
 
 /**
@@ -38,11 +38,18 @@ class Swisspost_YellowCube_Model_Queue_Message_Handler_Action_Processor_Order_Up
                 Mage::log($message . "\n" . print_r($response, true), Zend_Log::ERR, Swisspost_YellowCube_Helper_Data::YC_LOG_FILE, true);
                 $helperTools->sendAdminNotification($message);
 
-                // Add again in the queue to have an up to date status
-                $this->getQueue()->send(Zend_Json::encode(array(
-                    'action' => Swisspost_YellowCube_Model_Synchronizer::SYNC_ORDER_UPDATE,
-                    'order_id' => $data['order_id']
-                )));
+                if (empty($data['try'])) {
+                    $data['try'] = 0;
+                }
+
+                if (isset($data['try']) && $data['try'] < 5) {
+                    // Add again in the queue to have an up to date status
+                    $this->getQueue()->send(Zend_Json::encode(array(
+                        'action' => Swisspost_YellowCube_Model_Synchronizer::SYNC_ORDER_UPDATE,
+                        'order_id' => $data['order_id'],
+                        'try' => $data['try']++
+                    )));
+                }
             } else {
                 if (Mage::helper('swisspost_yellowcube')->getDebug()) {
                     Mage::log(print_r($response, true), Zend_Log::DEBUG, Swisspost_YellowCube_Helper_Data::YC_LOG_FILE);
@@ -82,11 +89,14 @@ class Swisspost_YellowCube_Model_Queue_Message_Handler_Action_Processor_Order_Up
             Mage::logException($e);
             // Let's keep going further processes
 
-            // Add again in the queue to have an up to date status
-            $this->getQueue()->send(Zend_Json::encode(array(
-                'action' => Swisspost_YellowCube_Model_Synchronizer::SYNC_ORDER_UPDATE,
-                'order_id' => $data['order_id']
-            )));
+            if (isset($data['try']) && $data['try'] < 5) {
+                // Add again in the queue to have an up to date status
+                $this->getQueue()->send(Zend_Json::encode(array(
+                    'action' => Swisspost_YellowCube_Model_Synchronizer::SYNC_ORDER_UPDATE,
+                    'order_id' => $data['order_id'],
+                    'try' => $data['try']++
+                )));
+            }
         }
 
         return $this;
