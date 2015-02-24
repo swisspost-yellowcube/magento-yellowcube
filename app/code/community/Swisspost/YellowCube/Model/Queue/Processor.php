848471261d800adfb5b670d8538df6be45cd195f
Sylvain Rayé <sly@diglin.com>
2015-02-24 22:06:20 +0100

    move log for processor higher in code to get the info ealier

--- app/code/community/Swisspost/YellowCube/Model/Queue/Processor.php
+++ app/code/community/Swisspost/YellowCube/Model/Queue/Processor.php
@@ -17,16 +17,17 @@ class Swisspost_YellowCube_Model_Queue_Processor
         /** @var Zend_Queue $queue */
         $queue = Mage::getModel('swisspost_yellowcube/queue')->getInstance();
         foreach ($queue->receive(100) as $message) {
+
+            if (Mage::helper('swisspost_yellowcube')->getDebug()) {
+                Mage::log($message->body, Zend_Log::DEBUG, Swisspost_YellowCube_Helper_Data::YC_LOG_FILE);
+            }
+
             try {
                 Mage::getSingleton('swisspost_yellowcube/queue_message_handler')->process(
                     Zend_Json::decode($message->body)
                 );
             } catch (Exception $e) {
                 Mage::logException($e);
-            }
-
-            if (Mage::helper('swisspost_yellowcube')->getDebug()) {
-                Mage::log($message->body, Zend_Log::DEBUG, Swisspost_YellowCube_Helper_Data::YC_LOG_FILE);
             }
 
             $queue->deleteMessage($message);
