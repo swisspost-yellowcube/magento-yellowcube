6b425f3387c61bf142ecd5fd14501682a6bd3a36
Janez Urevc <janez@janezurevc.name>
2017-11-08 16:01:01 +0100

    Add YellowCube status column to the admin product table.

--- app/code/community/Swisspost/YellowCube/Block/Adminhtml/Product/Grid.php
+++ app/code/community/Swisspost/YellowCube/Block/Adminhtml/Product/Grid.php
@@ -1,223 +1,46 @@
 <?php
 
+/**
+ * Alters default admin product table to add YellowCube column.
+ *
+ * Adds column that displays the YellowCube column on the product admin table.
+ */
 class Swisspost_YellowCube_Block_Adminhtml_Product_Grid extends Mage_Adminhtml_Block_Catalog_Product_Grid
 {
-    protected function _prepareCollection()
+
+    /**
+     * Overrides setCollection().
+     *
+     * We would ideally override _prepareCollection() but the collection gets
+     * loaded before we can alter it. In order to avoid this problem we
+     * override this function which will be eventually called by
+     * _prepareCollection() before the items will be loaded.
+     */
+    public function setCollection($collection)
     {
-        $store = $this->_getStore();
-        $collection = Mage::getModel('catalog/product')->getCollection()
-            ->addAttributeToSelect('sku')
-            ->addAttributeToSelect('yc_most_recent_expiration_date')
-            ->addAttributeToSelect('name')
-            ->addAttributeToSelect('attribute_set_id')
-            ->addAttributeToSelect('type_id');
-
-        //die(var_dump($collection));
-
-        if (Mage::helper('catalog')->isModuleEnabled('Mage_CatalogInventory')) {
-            $collection->joinField('qty',
-                'cataloginventory/stock_item',
-                'qty',
-                'product_id=entity_id',
-                '{{table}}.stock_id=1',
-                'left');
-        }
-        if ($store->getId()) {
-            //$collection->setStoreId($store->getId());
-            $adminStore = Mage_Core_Model_App::ADMIN_STORE_ID;
-            $collection->addStoreFilter($store);
-            $collection->joinAttribute(
-                'name',
-                'catalog_product/name',
-                'entity_id',
-                null,
-                'inner',
-                $adminStore
-            );
-            $collection->joinAttribute(
-                'custom_name',
-                'catalog_product/name',
-                'entity_id',
-                null,
-                'inner',
-                $store->getId()
-            );
-            $collection->joinAttribute(
-                'status',
-                'catalog_product/status',
-                'entity_id',
-                null,
-                'inner',
-                $store->getId()
-            );
-
-            $collection->joinAttribute(
-                'visibility',
-                'catalog_product/visibility',
-                'entity_id',
-                null,
-                'inner',
-                $store->getId()
-            );
-            $collection->joinAttribute(
-                'price',
-                'catalog_product/price',
-                'entity_id',
-                null,
-                'left',
-                $store->getId()
-            );
-        }
-        else {
-            $collection->addAttributeToSelect('price');
-            $collection->joinAttribute('status', 'catalog_product/status', 'entity_id', null, 'inner');
-            $collection->joinAttribute('visibility', 'catalog_product/visibility', 'entity_id', null, 'inner');
-        }
-
-        $this->setCollection($collection);
-
-        //parent::_prepareCollection();
-        $this->getCollection()->addWebsiteNamesToResult();
-        return $this;
+        $collection->addAttributeToSelect('yc_sync_with_yellowcube');
+        return parent::setCollection($collection);
     }
 
+    /**
+     * Overrides _prepareColumns().
+     *
+     * Adds YellowCube status column to the products admin table.
+     */
     protected function _prepareColumns()
     {
-        $this->addColumn('entity_id',
-            array(
-                'header'=> Mage::helper('catalog')->__('ID'),
-                'width' => '50px',
-                'type'  => 'number',
-                'index' => 'entity_id',
-            ));
-        $this->addColumn('name',
-            array(
-                'header'=> Mage::helper('catalog')->__('Name'),
-                'index' => 'name',
-            ));
+        parent::_prepareColumns();
+        $this->addColumn('yellowcube', array(
+            'header'=> Mage::helper('catalog')->__('YellowCube status'),
+            'width' => '40px',
+            'index' => 'yc_sync_with_yellowcube',
+            'type'  => 'options',
+            'options' => [
+                0 => Mage::helper('catalog')->__('Disabled'),
+                1 => Mage::helper('catalog')->__('Enabled'),
+            ],
+        ));
 
-        $store = $this->_getStore();
-        if ($store->getId()) {
-            $this->addColumn('custom_name',
-                array(
-                    'header'=> Mage::helper('catalog')->__('Name in %s', $store->getName()),
-                    'index' => 'custom_name',
-                ));
-        }
-
-        $this->addColumn('type',
-            array(
-                'header'=> Mage::helper('catalog')->__('Type'),
-                'width' => '60px',
-                'index' => 'type_id',
-                'type'  => 'options',
-                'options' => Mage::getSingleton('catalog/product_type')->getOptionArray(),
-            ));
-
-        $sets = Mage::getResourceModel('eav/entity_attribute_set_collection')
-            ->setEntityTypeFilter(Mage::getModel('catalog/product')->getResource()->getTypeId())
-            ->load()
-            ->toOptionHash();
-
-        $this->addColumn('set_name',
-            array(
-                'header'=> Mage::helper('catalog')->__('Attrib. Set Name'),
-                'width' => '100px',
-                'index' => 'attribute_set_id',
-                'type'  => 'options',
-                'options' => $sets,
-            ));
-
-        $this->addColumn('sku',
-            array(
-                'header'=> Mage::helper('catalog')->__('SKU'),
-                'width' => '80px',
-                'index' => 'sku',
-            ));
-
-        $this->addColumn('yc_most_recent_expiration_date',
-            array(
-                'header'=> Mage::helper('swisspost_yellowcube')->__('exp Date'), //todo find the right helper
-                'width' => '80px',
-                'index' => 'yc_most_recent_expiration_date',
-            ));
-
-        $store = $this->_getStore();
-        $this->addColumn('price',
-            array(
-                'header'=> Mage::helper('catalog')->__('Price'),
-                'type'  => 'price',
-                'currency_code' => $store->getBaseCurrency()->getCode(),
-                'index' => 'price',
-            ));
-
-        if (Mage::helper('catalog')->isModuleEnabled('Mage_CatalogInventory')) {
-            $this->addColumn('qty',
-                array(
-                    'header'=> Mage::helper('catalog')->__('Qty'),
-                    'width' => '100px',
-                    'type'  => 'number',
-                    'index' => 'qty',
-                ));
-        }
-
-        $this->addColumn('visibility',
-            array(
-                'header'=> Mage::helper('catalog')->__('Visibility'),
-                'width' => '70px',
-                'index' => 'visibility',
-                'type'  => 'options',
-                'options' => Mage::getModel('catalog/product_visibility')->getOptionArray(),
-            ));
-
-        $this->addColumn('status',
-            array(
-                'header'=> Mage::helper('catalog')->__('Status2222'),
-                'width' => '70px',
-                'index' => 'status',
-                'type'  => 'options',
-                'options' => Mage::getSingleton('catalog/product_status')->getOptionArray(),
-            ));
-
-
-
-        if (!Mage::app()->isSingleStoreMode()) {
-            $this->addColumn('websites',
-                array(
-                    'header'=> Mage::helper('catalog')->__('Websites'),
-                    'width' => '100px',
-                    'sortable'  => false,
-                    'index'     => 'websites',
-                    'type'      => 'options',
-                    'options'   => Mage::getModel('core/website')->getCollection()->toOptionHash(),
-                ));
-        }
-
-        $this->addColumn('action',
-            array(
-                'header'    => Mage::helper('catalog')->__('Action'),
-                'width'     => '50px',
-                'type'      => 'action',
-                'getter'     => 'getId',
-                'actions'   => array(
-                    array(
-                        'caption' => Mage::helper('catalog')->__('Edit'),
-                        'url'     => array(
-                            'base'=>'*/*/edit',
-                            'params'=>array('store'=>$this->getRequest()->getParam('store'))
-                        ),
-                        'field'   => 'id'
-                    )
-                ),
-                'filter'    => false,
-                'sortable'  => false,
-                'index'     => 'stores',
-            ));
-
-        if (Mage::helper('catalog')->isModuleEnabled('Mage_Rss')) {
-            $this->addRssList('rss/catalog/notifystock', Mage::helper('catalog')->__('Notify Low Stock RSS'));
-        }
-
-        return parent::_prepareColumns();
+        return $this;
     }
 }
